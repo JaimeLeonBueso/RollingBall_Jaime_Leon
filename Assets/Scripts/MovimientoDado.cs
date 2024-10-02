677581f4c7384b35cc17215b02d5481511f78dcf
JaimeLeonBueso <jaimeleonbueso@gmail.com>
2024-10-02 14:13:06 +0200

    Camara

--- Assets/Scripts/MovimientoDado.cs
+++ Assets/Scripts/MovimientoDado.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MovimientoDado : MonoBehaviour
 {
     [SerializeField] private float velocidad;
+    [SerializeField] private float fuerzaSalto;
+    [SerializeField] private float tipoMovimiento;
     private Rigidbody rb;
 
     private void Start()
@@ -14,23 +16,73 @@ public class MovimientoDado : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetAxis("Horizontal")>0)
-        {
-            rb.AddForce(Vector3.right * velocidad);
-        }
-        else if (Input.GetAxis("Horizontal")<0)
-        {
-            rb.AddForce(-Vector3.right * velocidad);
-        }
-        
-        if (Input.GetAxis("Vertical")>0)
-        {
-            rb.AddForce(Vector3.forward * velocidad);
-        }
-        else if (Input.GetAxis("Vertical")<0)
-        {
-            rb.AddForce(-Vector3.forward * velocidad);
+        //if (Input.GetAxis("Horizontal") > 0)
+        //{
+        //    rb.AddForce(Vector3.right * velocidad);
+        //}
+        //else if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") > 0)
+        //{
+        //    rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad);
+        //}
+        //if (Input.GetAxis("Horizontal") !=0 && Input.GetAxis("Vertical") !=0)
+        //{
+        //rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad);
+        //}
+        //else if (Input.GetAxis("Horizontal") ==0 && Input.GetAxis("Vertical") !=0)
+        //{
+        //    rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad);
+        //} 
+        //else if (Input.GetAxis("Horizontal") !=0 && Input.GetAxis("Vertical") ==0)
+        //{
+        //    rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad);
+        //}
+        //else if (Input.GetAxis("Horizontal") < 0)
+        //{
+        //    rb.AddForce(-Vector3.right * velocidad);
+        //}
+        //else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") > 0)
+        //{
+        //    rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad);
+        //} 
+        //else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") < 0)
+        //{
+        //    rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad);
+        //}
 
+        //if (Input.GetAxis("Vertical")>0)
+        //{
+        //    rb.AddForce(Vector3.forward * velocidad);
+        //}
+        //else if (Input.GetAxis("Vertical")<0)
+        //{
+        //    rb.AddForce(-Vector3.forward * velocidad);
+
+        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 1, 0) * fuerzaSalto, ForceMode.Impulse);
         }
-    }
+        if (tipoMovimiento == 0)
+        {
+            rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad);
+        } 
+        else if (tipoMovimiento == 1)
+        {
+            rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad,ForceMode.Impulse);
+        } 
+        else if (tipoMovimiento == 2)
+        {
+            rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad, ForceMode.VelocityChange);
+        } 
+        else if (tipoMovimiento == 3)
+        {
+            rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad, ForceMode.Force);
+        }
+        else if (tipoMovimiento == 4)
+        {
+            rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * velocidad, ForceMode.Acceleration);
+        }
+        }
 }
+   
+
